c9bb19ecb4a151e19c7a8785bab9262505d2dfba
Matheus787 <matheus787@hotmail.com>
2023-06-29 07:53:08 -0300

    separando o front em pasta separada

--- sistema-atendimento-front/src/app/app.component.spec.ts
+++ sistema-atendimento-front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sistema-atendimento'`, () => {
+  it(`should have as title 'sistema-atendimento-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sistema-atendimento');
+    expect(app.title).toEqual('sistema-atendimento-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sistema-atendimento app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sistema-atendimento-front app is running!');
   });
 });
