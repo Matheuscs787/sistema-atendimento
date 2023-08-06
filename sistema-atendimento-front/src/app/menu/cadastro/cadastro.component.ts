e4b663876d846ede709a16ed04dd54fc95a4f89c
Matheus787 <matheus787@hotmail.com>
2023-08-06 17:33:12 -0300

    gitflow-feature-stash: atividade-13
    atividade13

--- sistema-atendimento-front/src/app/menu/cadastro/cadastro.component.ts
+++ sistema-atendimento-front/src/app/menu/cadastro/cadastro.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Paciente } from 'src/app/inteface/paciente.model';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -8,29 +11,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./cadastro.component.css']
 })
 
-export class CadastroComponent{
+export class CadastroComponent {
   myForm: FormGroup;
-  codigo: string = '';
-  dataCadastro: string = '';
-  nome: string = '';
-  nascimento: string = '';
-  cpf: string = '';
-  estadoCivil: string = '';
-  telefone: string = '';
-  telefone1: string = '';
-  cep: string = '';
-  cidade: string = '';
-  uf: string = '';
-  bairro: string = '';
-  rua: string = '';
-  numero: string = '';
-  complemento: string = '';
+  paciente: Paciente = {
+    id: '',
+    dataCadastro: '',
+    nome: '',
+    nascimento: '',
+    cpf: '',
+    estadoCivil: '',
+    telefone: '',
+    telefone1: '',
+    cep: '',
+    cidade: '',
+    uf: '',
+    bairro: '',
+    rua: '',
+    numero: '',
+    complemento: ''
+  };
 
-  constructor(private router: Router, private formBuilder: FormBuilder){
+  constructor(private router: Router, private formBuilder: FormBuilder, private http: HttpClient) {
     this.myForm = this.formBuilder.group({
+      id: [''],
+      dataCadastro: [''],
       nome: ['', Validators.required],
       nascimento: ['', Validators.required],
-      cpf: ['', Validators.maxLength(11), Validators.minLength(11), Validators.required],
+      cpf: ['', [Validators.maxLength(11), Validators.minLength(11), Validators.required]],
       estadoCivil: ['', Validators.required],
       telefone: ['', Validators.required],
       telefone1: ['', Validators.required],
@@ -39,8 +46,12 @@ export class CadastroComponent{
       uf: ['', Validators.required],
       bairro: ['', Validators.required],
       rua: ['', Validators.required],
-      numero: ['', Validators.required]
+      numero: ['', Validators.required],
+      complemento: ['']
     });
+
+    const dataAtual = new Date().toLocaleDateString('pt-BR');
+    this.myForm.get('dataCadastro')?.patchValue(dataAtual);
   }
 
   ngAfterViewInit() {
@@ -48,21 +59,47 @@ export class CadastroComponent{
     M.FormSelect.init(selectElems);
   }
 
-  ngOnInit() {}
-
   submitForm() {
-    console.log('Formulário enviado:', this);
+    if (this.myForm.valid) {
+      const codigo = this.myForm.get('id')?.value;
+      if (!codigo) {
+        const dataAtual = new Date();
+        const formattedDataAtual = this.formatDate(dataAtual);
+        this.myForm.get('dataCadastro')?.setValue(formattedDataAtual);
+
+        const paciente = this.myForm.value;
+        this.http.post<any>('http://localhost:3000/paciente', paciente).subscribe(
+          (response) => {
+            this.myForm.patchValue({ id: response.id });
+            this.myForm.patchValue({ dataCadastro: response.dataCadastro });
+          },
+          (error) => {
+            alert('Erro ao salvar paciente:' + JSON.stringify(error));
+          }
+        );
+      } else {
+        alert('Formulário inválido. Verifique os campos.');
+      }
+    }
   }
 
-  get formControls(){
+  get formControls() {
     return this.myForm.controls;
   }
 
-  prontuarios(){
-    if (this.codigo) {
-      this.router.navigate(['/prontuarios', this.codigo]);
+  prontuarios() {
+    if (this.paciente.id !== '') {
+      this.router.navigate(['/prontuarios', this.paciente.id]);
     } else {
       alert('Só é possível abrir prontuários de pacientes cadastrados!');
     }
   }
+
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const day = `${date.getDate()}`.padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
 }
